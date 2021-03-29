18cdd3c3fd005c6cf494d30367880fe1683ec342
sebas1899 <81540156+sebas1899@users.noreply.github.com>
2021-03-28 23:12:35 -0500

    Create Caso1.java

---
+++ src/com/senati/eti/Caso1.java
@@ -0,0 +1,20 @@
+package com.senati.eti;
+
+public class Caso1 {
+
+	public static void main(String[] args) {
+		int i = 1;
+		
+		while (i <= 5) {
+			System.out.println("Bienvenido al curso: " + i);
+			
+			i++;
+			
+		}
+		
+		
+		
+
+	}
+
+}
